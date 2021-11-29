bb1b9dfde7905cca1632e972122f578740ee45e1
lukaskronus <79519320+lukaskronus@users.noreply.github.com>
2021-11-29 00:19:48 +0000

    Create shorten-link.js

---
+++ shorten-link.js
@@ -0,0 +1,130 @@
+const config = {
+no_ref: "off", //Control the HTTP referrer header, if you want to create an anonymous link that will hide the HTTP Referer header, please set to "on" .
+theme:"",//Homepage theme, use the empty value for default theme. To use urlcool theme, please fill with "theme/urlcool" .
+cors: "on",//Allow Cross-origin resource sharing for API requests.
+}
+
+const html404 = `<!DOCTYPE html>
+<body>
+  <h1>404 Not Found.</h1>
+  <p>The url you visit is not found.</p>
+  <a href="https://github.com/xyTom/Url-Shorten-Worker/" target="_self">Fork me on GitHub</a>
+</body>`
+
+let response_header={
+  "content-type": "text/html;charset=UTF-8",
+} 
+
+if (config.cors=="on"){
+  response_header={
+  "content-type": "text/html;charset=UTF-8",
+  "Access-Control-Allow-Origin":"*",
+  "Access-Control-Allow-Methods": "POST",
+  }
+}
+
+async function randomString(len) {
+　　len = len || 6;
+　　let $chars = 'ABCDEFGHJKMNPQRSTWXYZabcdefhijkmnprstwxyz2345678';    /****默认去掉了容易混淆的字符oOLl,9gq,Vv,Uu,I1****/
+　　let maxPos = $chars.length;
+　　let result = '';
+　　for (i = 0; i < len; i++) {
+　　　　result += $chars.charAt(Math.floor(Math.random() * maxPos));
+　　}
+　　return result;
+}
+async function checkURL(URL){
+    let str=URL;
+    let Expression=/http(s)?:\/\/([\w-]+\.)+[\w-]+(\/[\w- .\/?%&=]*)?/;
+    let objExp=new RegExp(Expression);
+    if(objExp.test(str)==true){
+      if (str[0] == 'h')
+        return true;
+      else
+        return false;
+    }else{
+        return false;
+    }
+} 
+async function save_url(URL){
+    let random_key=await randomString()
+    let is_exist=await LINKS.get(random_key)
+    console.log(is_exist)
+    if (is_exist == null)
+        return await LINKS.put(random_key, URL, {expirationTtl: 86400}),random_key
+    else
+        save_url(URL)
+}
+async function handleRequest(request) {
+  console.log(request)
+  if (request.method === "POST") {
+    let req=await request.json()
+    console.log(req["url"])
+    if(!await checkURL(req["url"])){
+    return new Response(`{"status":500,"key":": Error: Url illegal."}`, {
+      headers: response_header,
+    })}
+    let stat,random_key=await save_url(req["url"])
+    console.log(stat)
+    if (typeof(stat) == "undefined"){
+      return new Response(`{"status":200,"key":"/`+random_key+`"}`, {
+      headers: response_header,
+    })
+    }else{
+      return new Response(`{"status":200,"key":": Error:Reach the KV write limitation."}`, {
+      headers: response_header,
+    })}
+  }else if(request.method === "OPTIONS"){  
+      return new Response(``, {
+      headers: response_header,
+    })
+
+  }
+
+  const requestURL = new URL(request.url)
+  const path = requestURL.pathname.split("/")[1]
+  console.log(path)
+  if(!path){
+
+    const html= await fetch("https://xytom.github.io/Url-Shorten-Worker/"+config.theme+"/index.html")
+    
+    return new Response(await html.text(), {
+    headers: {
+      "content-type": "text/html;charset=UTF-8",
+    },
+  })
+  }
+  const value = await LINKS.get(path)
+  console.log(value)
+  
+
+  const location = value
+  if (location) {
+    if (config.no_ref=="on"){
+      let no_ref= await fetch("https://xytom.github.io/Url-Shorten-Worker/no-ref.html")
+      no_ref=await no_ref.text()
+      no_ref=no_ref.replace(/{Replace}/gm, location)
+      return new Response(no_ref, {
+      headers: {
+        "content-type": "text/html;charset=UTF-8",
+      },
+    })
+    }else{
+      return Response.redirect(location, 302)
+    }
+    
+  }
+  // If request not in kv, return 404
+  return new Response(html404, {
+    headers: {
+      "content-type": "text/html;charset=UTF-8",
+    },
+    status: 404
+  })
+}
+
+
+
+addEventListener("fetch", async event => {
+  event.respondWith(handleRequest(event.request))
+})
