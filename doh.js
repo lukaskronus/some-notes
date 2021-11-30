0436cc36f9fd21d2ae260e17bd158d3ec10bce35
lukaskronus <79519320+lukaskronus@users.noreply.github.com>
2021-11-30 01:13:15 +0000

    Create doh.js

---
+++ doh.js
@@ -0,0 +1,31 @@
+addEventListener('fetch', function(event) {
+    const { request } = event
+    const response = handleRequest(request)
+    event.respondWith(response)
+})
+
+async function handleRequest(request) {
+    const doh = 'https://sg.adhole.org/dns-query'
+    const contype = 'application/dns-message'
+    const { method, headers, url } = request
+    const { host, searchParams } = new URL(url)
+    if (method == 'GET' && searchParams.has('dns')) {
+        return await fetch(doh + '?dns=' + searchParams.get('dns'), {
+            method: 'GET',
+            headers: {
+                'Accept': contype,
+            }
+        });
+    } else if (method == 'POST' && headers.get('content-type')=='application/dns-message') {
+        return await fetch(doh, {
+            method: 'POST',
+            headers: {
+                'Accept': contype,
+                'Content-Type': contype,
+            },
+            body: await request.arrayBuffer()
+        });
+    } else {
+        return new Response("", {status: 404})
+    }
+}
