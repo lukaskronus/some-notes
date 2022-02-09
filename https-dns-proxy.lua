6a5fa9105488d660dae02a2195b317d62e16a478
lukaskronus <79519320+lukaskronus@users.noreply.github.com>
2022-02-09 01:21:34 +0000

    Update https-dns-proxy.lua

--- https-dns-proxy.lua
+++ https-dns-proxy.lua
@@ -2,7 +2,7 @@
 return {
         name = "Blitz AhaDNS",
         label = _("Blitz AhaDNS"),
-        resolver_url = "https://blitz.ahadns.com/1:1.10.11.18.20.22.23.26",
+        resolver_url = "https://blitz.ahadns.com/1:1.5.7.20",
         bootstrap_dns = "1.1.1.1,1.0.0.1,2606:4700:4700::1111,2606:4700:4700::1001,8.8.8.8,8.8.4.4,2001:4860:4860::8888,2001:4860:4860::8844"
 }
 
@@ -10,6 +10,6 @@ return {
 return {
         name = "basic.rethinkdns.com",
         label = _("Rethink DNS (Configurable)"),
-        resolver_url = "https://basic.rethinkdns.com/1:cAAAASAAAAg=",
+        resolver_url = "https://basic.rethinkdns.com/1:YAqAAQCAACEgAA==",
         bootstrap_dns = "1.1.1.1,1.0.0.1,2606:4700:4700::1111,2606:4700:4700::1001,8.8.8.8,8.8.4.4,2001:4860:4860::8888,2001:4860:4860::8844"
 }
