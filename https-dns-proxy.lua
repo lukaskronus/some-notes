210f7ed1ee4a24ebc4258c774f508e5865058841
lukaskronus <79519320+lukaskronus@users.noreply.github.com>
2022-02-09 12:06:49 +0000

    Update https-dns-proxy.lua

--- https-dns-proxy.lua
+++ https-dns-proxy.lua
@@ -10,6 +10,6 @@ return {
 return {
         name = "basic.rethinkdns.com",
         label = _("Rethink DNS (Configurable)"),
-        resolver_url = "https://basic.rethinkdns.com/1:YAqAAQCAACEgAA==",
+        resolver_url = "https://basic.rethinkdns.com/1:YAIAgAAxIAA=",
         bootstrap_dns = "1.1.1.1,1.0.0.1,2606:4700:4700::1111,2606:4700:4700::1001,8.8.8.8,8.8.4.4,2001:4860:4860::8888,2001:4860:4860::8844"
 }
