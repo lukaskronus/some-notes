ca030062aa23b6098677465d52a3217804ff7aeb
lukaskronus <79519320+lukaskronus@users.noreply.github.com>
2022-02-10 05:11:17 +0000

    Update https-dns-proxy.lua

--- https-dns-proxy.lua
+++ https-dns-proxy.lua
@@ -2,7 +2,7 @@
 return {
         name = "Blitz AhaDNS",
         label = _("Blitz AhaDNS"),
-        resolver_url = "https://blitz.ahadns.com/1:1.5.7.20",
+        resolver_url = "https://blitz.ahadns.com/1:1.5.7.10.11.20.26",
         bootstrap_dns = "1.1.1.1,1.0.0.1,2606:4700:4700::1111,2606:4700:4700::1001,8.8.8.8,8.8.4.4,2001:4860:4860::8888,2001:4860:4860::8844"
 }
 
