dca76c62c085d6ec9422a7bf5fb59fc6edf42adf
lukaskronus <79519320+lukaskronus@users.noreply.github.com>
2022-02-17 12:36:42 +0000

    Update https-dns-proxy.lua

--- https-dns-proxy.lua
+++ https-dns-proxy.lua
@@ -13,3 +13,6 @@ return {
         resolver_url = "https://basic.rethinkdns.com/1:YAIAgAAxIAA=",
         bootstrap_dns = "1.1.1.1,1.0.0.1,2606:4700:4700::1111,2606:4700:4700::1001,8.8.8.8,8.8.4.4,2001:4860:4860::8888,2001:4860:4860::8844"
 }
+
+### RethinkDNS Alternative ###
+Link: https://fukurodns.deno.dev/1:4A6AAQQAAIAAAQAxKAA=
