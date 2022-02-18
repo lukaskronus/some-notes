24bda8b723fb326eddc69673c8f33bf0203a5961
lukaskronus <79519320+lukaskronus@users.noreply.github.com>
2022-02-18 12:23:11 +0000

    Change resolver DNS url
    Fix Google Fonts does not load in some websites

--- https-dns-proxy.lua
+++ https-dns-proxy.lua
@@ -2,7 +2,7 @@
 return {
         name = "Blitz AhaDNS",
         label = _("Blitz AhaDNS"),
-        resolver_url = "https://blitz.ahadns.com/1:1.5.7.10.11.20.26",
+        resolver_url = "https://blitz.ahadns.com/1:1.5.7.10.11.17.18.20.26",
         bootstrap_dns = "1.1.1.1,1.0.0.1,2606:4700:4700::1111,2606:4700:4700::1001,8.8.8.8,8.8.4.4,2001:4860:4860::8888,2001:4860:4860::8844"
 }
 
@@ -10,7 +10,7 @@ return {
 return {
         name = "basic.rethinkdns.com",
         label = _("Rethink DNS (Configurable)"),
-        resolver_url = "https://basic.rethinkdns.com/1:YAIAgAAxIAA=",
+        resolver_url = "https://basic.rethinkdns.com/1:IB6AAIABBAAAgCAA",
         bootstrap_dns = "1.1.1.1,1.0.0.1,2606:4700:4700::1111,2606:4700:4700::1001,8.8.8.8,8.8.4.4,2001:4860:4860::8888,2001:4860:4860::8844"
 }
 
