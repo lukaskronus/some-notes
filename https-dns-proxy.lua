31353dcc5affdb2607b3f7bcf01a61611b06720f
lukaskronus <79519320+lukaskronus@users.noreply.github.com>
2022-03-05 11:24:04 +0000

    Allow Camera P2P

--- https-dns-proxy.lua
+++ https-dns-proxy.lua
@@ -7,7 +7,7 @@ return {
 return {
         name = "serverless.dns.deno",
         label = _("ServerlessDNS Deno"),
-        resolver_url = "https://kagentsuki.deno.dev/1:IB6AAIABBAAAgCAA",
+        resolver_url = "https://kagentsuki.deno.dev/1:4BSAAAYACAAAMSAA",
         bootstrap_dns = "1.1.1.1,1.0.0.1,2606:4700:4700::1111,2606:4700:4700::1001"
 }
 return {
@@ -19,6 +19,6 @@ return {
 return {
         name = "blitz.ahadns.com",
         label = _("Blitz AhaDNS"),
-        resolver_url = "https://blitz.ahadns.com/1:1.5.7.10.11.18.20.26",
+        resolver_url = "https://blitz.ahadns.com/1:1.4.7.20.22.23.26",
         bootstrap_dns = "1.1.1.1,1.0.0.1,2606:4700:4700::1111,2606:4700:4700::1001"
 }
