fef6708198f120854dd5840005a198e9ea32f80a
lukaskronus <79519320+lukaskronus@users.noreply.github.com>
2022-05-04 17:06:06 +0700

    Update https-dns-proxy.lua

--- https-dns-proxy.lua
+++ https-dns-proxy.lua
@@ -2,29 +2,41 @@ return {
         name = "fukuro.dns.japan",
         label = _("Fukuro DNS Japan"),
         resolver_url = "https://doh.tweensora.jp.eu.org/dns-query",
-        bootstrap_dns = "1.1.1.1,1.0.0.1,2606:4700:4700::1111,2606:4700:4700::1001"
+        bootstrap_dns = "1.1.1.1,1.0.0.1,2606:4700:4700::1111,2606:4700:4700::1001,8.8.8.8,8.8.4.4,2001:4860:4860::8888,2001:4860:4860::8844"
 }
 return {
         name = "serverless.dns.deno",
         label = _("ServerlessDNS Deno"),
         resolver_url = "https://headoffice2.deno.dev/1:4BaAAAYAABCAAAABIAA=",
-        bootstrap_dns = "1.1.1.1,1.0.0.1,2606:4700:4700::1111,2606:4700:4700::1001"
+        bootstrap_dns = "1.1.1.1,1.0.0.1,2606:4700:4700::1111,2606:4700:4700::1001,8.8.8.8,8.8.4.4,2001:4860:4860::8888,2001:4860:4860::8844"
 }
 return {
         name = "workers.cloudflare.serverless",
         label = _("RethinkDNS CF Worker"),
         resolver_url = "https://serverless-dns.forgotten.workers.dev/1:4BaAAAYAABCAAAABIAA=",
-        bootstrap_dns = "1.1.1.1,1.0.0.1,2606:4700:4700::1111,2606:4700:4700::1001"
+        bootstrap_dns = "1.1.1.1,1.0.0.1,2606:4700:4700::1111,2606:4700:4700::1001,8.8.8.8,8.8.4.4,2001:4860:4860::8888,2001:4860:4860::8844"
 }
 return {
         name = "blitz.ahadns.com",
         label = _("Blitz AhaDNS"),
         resolver_url = "https://blitz.ahadns.com/1:1.4.7.10.11.12.20.22.23.26",
-        bootstrap_dns = "1.1.1.1,1.0.0.1,2606:4700:4700::1111,2606:4700:4700::1001"
+        bootstrap_dns = "1.1.1.1,1.0.0.1,2606:4700:4700::1111,2606:4700:4700::1001,8.8.8.8,8.8.4.4,2001:4860:4860::8888,2001:4860:4860::8844"
 }
 return {
         name = "chuoixam.dns.singapore",
         label = _("Chuoixam DNS"),
         resolver_url = "https://agh.chuoixam.eu.org/dns-query",
-        bootstrap_dns = "1.1.1.1,1.0.0.1,2606:4700:4700::1111,2606:4700:4700::1001"
+        bootstrap_dns = "1.1.1.1,1.0.0.1,2606:4700:4700::1111,2606:4700:4700::1001,8.8.8.8,8.8.4.4,2001:4860:4860::8888,2001:4860:4860::8844"
+}
+return {
+        name = "my.nextdns.io",
+        label = _("Next DNS"),
+        resolver_url = "https://dns.nextdns.io/a5fddf",
+        bootstrap_dns = "45.90.28.49,45.90.30.49"
+}
+return {
+	name = "doh-sg.blahdns.com",
+	label = _("BlahDNS - SG"),
+	resolver_url = "https://doh-sg.blahdns.com/dns-query",
+	bootstrap_dns = "1.1.1.1,1.0.0.1,2606:4700:4700::1111,2606:4700:4700::1001,8.8.8.8,8.8.4.4,2001:4860:4860::8888,2001:4860:4860::8844"
 }
