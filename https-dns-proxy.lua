f35966e1d60668cccfec40ee1f3e47a027a631f4
lukaskronus <79519320+lukaskronus@users.noreply.github.com>
2022-05-04 19:53:44 +0700

    Update deno DNS

--- https-dns-proxy.lua
+++ https-dns-proxy.lua
@@ -7,7 +7,7 @@ return {
 return {
         name = "serverless.dns.deno",
         label = _("ServerlessDNS Deno"),
-        resolver_url = "https://headoffice2.deno.dev/1:4BaAAAYAABCAAAABIAA=",
+        resolver_url = "https://fukurodns.deno.dev/1:4BaAAAYAABCAAAABIAA=",
         bootstrap_dns = "1.1.1.1,1.0.0.1,2606:4700:4700::1111,2606:4700:4700::1001,8.8.8.8,8.8.4.4,2001:4860:4860::8888,2001:4860:4860::8844"
 }
 return {
