f2b787a0c0a0726a21872835582eb0838879d71d
lukaskronus <79519320+lukaskronus@users.noreply.github.com>
2022-05-27 10:02:23 +0700

    Update deno link

--- https-dns-proxy.lua
+++ https-dns-proxy.lua
@@ -7,7 +7,7 @@ return {
 return {
         name = "serverless.dns.deno",
         label = _("ServerlessDNS Deno"),
-        resolver_url = "https://fukurodns.deno.dev/1:oAYGAAAQgAAgAA==",
+        resolver_url = "https://denodns.deno.dev/1:oAYGAAAQgAAgAA==",
         bootstrap_dns = "1.1.1.1,1.0.0.1,2606:4700:4700::1111,2606:4700:4700::1001,8.8.8.8,8.8.4.4,2001:4860:4860::8888,2001:4860:4860::8844"
 }
 return {
