421a54a0fed44be3ceb00991b3b6a07de40826bb
lukaskronus <79519320+lukaskronus@users.noreply.github.com>
2022-06-28 06:08:36 +0700

    Update blocklists

--- https-dns-proxy.lua
+++ https-dns-proxy.lua
@@ -7,13 +7,13 @@ return {
 return {
         name = "serverless.dns.deno",
         label = _("ServerlessDNS Deno"),
-        resolver_url = "https://denodns.deno.dev/1:oAYGAAAQgAAgAA==",
+        resolver_url = "https://denodns.deno.dev/1:4AYCAAAZgAAAUCAA",
         bootstrap_dns = "1.1.1.1,1.0.0.1,2606:4700:4700::1111,2606:4700:4700::1001,8.8.8.8,8.8.4.4,2001:4860:4860::8888,2001:4860:4860::8844"
 }
 return {
         name = "workers.cloudflare.serverless",
         label = _("RethinkDNS CF Worker"),
-        resolver_url = "https://serverless-dns.forgotten.workers.dev/1:oAYGAAAQgAAgAA==",
+        resolver_url = "https://serverless-dns.forgotten.workers.dev/1:4AYCAAAZgAAAUCAA",
         bootstrap_dns = "1.1.1.1,1.0.0.1,2606:4700:4700::1111,2606:4700:4700::1001,8.8.8.8,8.8.4.4,2001:4860:4860::8888,2001:4860:4860::8844"
 }
 return {
