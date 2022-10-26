27f65a913644c64c00932de2ff2100c373123fa3
lukaskronus <79519320+lukaskronus@users.noreply.github.com>
2022-10-26 22:00:07 +0700

    Update https-dns-proxy.lua
    - Remove VietDNS due to DNSSEC failure
    - Update deno filter lists
    - Update AhaDNS Blitz link

--- https-dns-proxy.lua
+++ https-dns-proxy.lua
@@ -7,13 +7,13 @@ return {
 return {
         name = "serverless.dns.deno",
         label = _("ServerlessDNS Deno"),
-        resolver_url = "https://denodns.deno.dev/1:4AIAmJABAHDgPw==",
+        resolver_url = "https://denodns.deno.dev/1:4AIAjZAAAHDgPw==",
         bootstrap_dns = "1.1.1.1,1.0.0.1,2606:4700:4700::1111,2606:4700:4700::1001,8.8.8.8,8.8.4.4,2001:4860:4860::8888,2001:4860:4860::8844"
 }
 return {
         name = "blitz.ahadns.com",
         label = _("Blitz AhaDNS"),
-        resolver_url = "https://blitz.ahadns.com/1:1.5.7.8.17.20.22.23",
+        resolver_url = "https://blitz.ahadns.com/1:1.5.7.8.12.17.20.22.23.26",
         bootstrap_dns = "1.1.1.1,1.0.0.1,2606:4700:4700::1111,2606:4700:4700::1001,8.8.8.8,8.8.4.4,2001:4860:4860::8888,2001:4860:4860::8844"
 }
 return {
@@ -21,10 +21,4 @@ return {
         label = _("Next DNS"),
         resolver_url = "https://dns.nextdns.io/b4cc75",
         bootstrap_dns = "45.90.28.0,45.90.30.0"
-}
-return {
-	name = "vietdns.vn",
-	label = _("VIETDNS"),
-	resolver_url = "https://dns.vietdns.vn",
-	bootstrap_dns = "103.151.57.2,103.151.56.2"
 }
