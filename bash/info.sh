36840df2dfcffb837256229ceaad7a47ba6275b4
lukaskronus <79519320+lukaskronus@users.noreply.github.com>
2022-11-22 14:03:48 +0700

    Delete info.sh

--- bash/info.sh
+++
@@ -1,4 +0,0 @@
-curl -X GET "https://api.cloudflare.com/client/v4/accounts/$CF_ID/access/users" \
-     -H "X-Auth-Email: $CF_AC" \
-     -H "X-Auth-Key: $CF_TOKEN" \
-     -H "Content-Type: application/json"
