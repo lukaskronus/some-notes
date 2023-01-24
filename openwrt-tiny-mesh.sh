2adc0476d81abf1a7f19b750306eae8f908d63da
lukaskronus <79519320+lukaskronus@users.noreply.github.com>
2023-01-24 17:59:47 +0700

    Update openwrt-tiny-mesh.sh

--- openwrt-tiny-mesh.sh
+++ openwrt-tiny-mesh.sh
@@ -1,9 +1,4 @@
+# OpenWRT 18.06
 make image PROFILE=tplink_tl-wr841-v11 PACKAGES="uhttpd uhttpd-mod-ubus libiwinfo-lua luci-base luci-mod-admin-full \
-wpad -wpad-mini -wpad-basic-wolfssl \
-luci-theme-material -luci-theme-bootstrap \
--firewall -luci-app-firewall -ppp -ppp-mod-pppoe \
--ip6tables -odhcp6c -kmod-ipv6 -kmod-ip6tables -odhcpd-ipv6only \
--dnsmasq -odhcpd -iptables \
--kmod-ipt-conntrack -kmod-ipt-core -kmod-ipt-nat -kmod-ipt-offload \
--kmod-nf-conntrack -kmod-nf-conntrack6 -kmod-nf-flow -kmod-nf-ipt -kmod-nf-nat -kmod-nf-reject \
--ca-bundle -opkg -luci-app-opkg"
+wpad kmod-batman-adv batctl -wpad-mini luci-theme-material -luci-theme-bootstrap -firewall -luci-app-firewall \
+-ppp -ppp-mod-pppoe -ip6tables -odhcp6c -kmod-ipv6 -kmod-ip6tables -odhcpd-ipv6only -dnsmasq -odhcpd -iptables"
