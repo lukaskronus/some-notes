039eef52c17951c9034bc8b03b8a985ef0b52b56
lukaskronus <79519320+lukaskronus@users.noreply.github.com>
2023-04-14 08:55:25 +0700

    Add nextdns router IP

--- openwrt-tiny-mesh.sh
+++ openwrt-tiny-mesh.sh
@@ -1,4 +1,4 @@
 # OpenWRT 18.06
 make image PROFILE=tplink_tl-wr841-v11 PACKAGES="uhttpd uhttpd-mod-ubus libiwinfo-lua luci-base luci-mod-admin-full \
-wpad kmod-batman-adv batctl -wpad-mini luci-theme-material -luci-theme-bootstrap -firewall -luci-app-firewall \
+wpad kmod-batman-adv batctl-full -wpad-mini luci-theme-material -luci-theme-bootstrap -firewall -luci-app-firewall \
 -ppp -ppp-mod-pppoe -ip6tables -odhcp6c -kmod-ipv6 -kmod-ip6tables -odhcpd-ipv6only -dnsmasq -odhcpd -iptables"
