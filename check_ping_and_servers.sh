08ee68db7b00a6b3d1301a9befb1d935d8be659e
lukaskronus <79519320+lukaskronus@users.noreply.github.com>
2023-06-28 18:32:41 +0700

    Update check_ping_and_servers.sh

--- check_ping_and_servers.sh
+++ check_ping_and_servers.sh
@@ -1,45 +1,37 @@
 #!/bin/sh
-# WIP
+
 check_ping () {
   server="$1"
   interface="$2"
-  ping_result=$(ping -c 1 -I "$interface" -W 10 "$server" | grep 'time=' | awk -F 'time=' '{print $2}' | awk '{print $1}')
-  ping_time=$(printf "%.0f" "$ping_result")
+  ping_time=$(printf "%.0f" "$(ping -c 1 -I "$interface" -W 10 "$server" | grep 'time=' | awk -F 'time=' '{print $2}' | awk '{print $1}')")
 }
 
 find_fastest_server () {
   server_list="/etc/servers"
-  fastest_server=""
-  fastest_ping=-1
-  # Replace with the name of your WAN interface
-  interface="wan"
+  fastest_ping=100
+  fastest_server=
+  interface="br-lan"
 
-  while read -r server; do
-    ping_result=$(check_ping "$server" "$interface")
-    if [  $fastest_ping -eq -1 ] || [ "$ping_result" -lt "$fastest_ping" ]; then
+  while IFS= read -r server; do
+    check_ping "$server" "$interface"
+    if [ -n "$ping_time" ] && [ "$ping_time" -lt "$fastest_ping" ]; then
       fastest_server="$server"
-      fastest_ping="$ping_result"
+      fastest_ping="$ping_time"
+      sleep 2
     fi
   done < "$server_list"
 }
 
-interface="nordlynx"
-if [ "$(check_ping "google.com" "$interface")" -lt 50 ]; then
+ifname="nordlynx"
+check_ping "google.com" "$ifname"
+# Check if VPN ping is lower than 50ms
+if [ "$ping_time" -lt 50 ]; then
   exit 0    
 else
-  fastest_server=""
   find_fastest_server
-  uci set network.${interface}.endpoint_host="$fastest_server"
-  if ! uci commit network; then
-    # Error handling on commit network
-    echo "Error: Failed to commit network changes"
-    exit 1
-  fi
-  if ! /etc/init.d/network restart; then
-    # Error handling on commit network
-    echo "Error: Failed to restart the network"
-    exit 1
-  fi
+  uci set network.@wireguard_${ifname}[-1].endpoint_host="$fastest_server"
+  uci commit network
+  /etc/init.d/network restart
 fi
 
 exit 0
