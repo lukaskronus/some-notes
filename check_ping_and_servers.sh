04564c9f6a0bcfc9e71f0217b39e55515ac900e2
lukaskronus <79519320+lukaskronus@users.noreply.github.com>
2023-06-30 05:59:30 +0700

    Update check_ping_and_servers.sh

--- check_ping_and_servers.sh
+++ check_ping_and_servers.sh
@@ -19,6 +19,7 @@ find_fastest_server () {
   server_list="/etc/servers"
   fastest_ping=100
   fastest_server=
+  # Change to your default Interface
   interface="br-lan"
   while IFS= read -r server; do
     check_ping "$server" "$interface" &>/dev/null
@@ -28,20 +29,24 @@ find_fastest_server () {
     fi
   done < "$server_list"
 }
-
+# Chaang to your VPN interface
 ifname="nordlynx"
 check_ping "8.8.8.8" "$ifname" &>/dev/null
 
 # Check if VPN ping is lower than 50ms
-if [ "$ping_time" -eq 0 ] || [ "$ping_time" -lt 10 ]; then
+if [ "$ping_time" -eq 0 ] || [ "$ping_time" -lt 50 ]; then
   exit 0
 else
   find_fastest_server
   echo "Fastest server: $fastest_server"
   echo "Fastest ping: $fastest_ping ms"
-  uci set network.@wireguard_${ifname}[-1].endpoint_host="$fastest_server"
-  uci commit network
-  /etc/init.d/network restart
+  if [ -n "$fastest_server" ] && [ ! "$fastest_ping" -eq 0 ]; then
+    uci set network.@wireguard_${ifname}[-1].endpoint_host="$fastest_server"
+    uci commit network
+    /etc/init.d/network restart
+  else
+    exit 0
+  fi
 fi
 
 exit 0
