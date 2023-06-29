6b380757821226b046deb112667e591576c769d8
lukaskronus <79519320+lukaskronus@users.noreply.github.com>
2023-06-29 15:16:47 +0700

    Update check_ping_and_servers.sh

--- check_ping_and_servers.sh
+++ check_ping_and_servers.sh
@@ -3,7 +3,16 @@
 check_ping () {
   server="$1"
   interface="$2"
-  ping_time=$(printf "%.0f" "$(ping -c 1 -I "$interface" -W 10 "$server" | grep 'time=' | awk -F 'time=' '{print $2}' | awk '{print $1}')")
+  count=0
+  while [ "$count" -lt 5 ]; do
+    ping_output=$(ping "$server" -c 1 -I "$interface" -W 5)
+    ping_time=$(printf "%.0f" "$(echo "$ping_output" | grep 'time=' | awk -F 'time=' '{print $2}' | awk '{print $1}')")
+    if [ -n "$ping_time" ] && [ "$ping_time" -gt 0 ]; then
+      break
+    fi
+    count=$((count+1))
+    sleep 1
+  done
 }
 
 find_fastest_server () {
@@ -11,24 +20,25 @@ find_fastest_server () {
   fastest_ping=100
   fastest_server=
   interface="br-lan"
-
   while IFS= read -r server; do
-    check_ping "$server" "$interface"
-    if [ -n "$ping_time" ] && [ "$ping_time" -lt "$fastest_ping" ]; then
+    check_ping "$server" "$interface" &>/dev/null
+    if [ "$ping_time" -lt "$fastest_ping" ]; then
       fastest_server="$server"
       fastest_ping="$ping_time"
-      sleep 2
     fi
   done < "$server_list"
 }
 
 ifname="nordlynx"
-check_ping "google.com" "$ifname"
+check_ping "8.8.8.8" "$ifname" &>/dev/null
+
 # Check if VPN ping is lower than 50ms
-if [ "$ping_time" -lt 50 ]; then
-  exit 0    
+if [ "$ping_time" -eq 0 ] || [ "$ping_time" -lt 10 ]; then
+  exit 0
 else
   find_fastest_server
+  echo "Fastest server: $fastest_server"
+  echo "Fastest ping: $fastest_ping ms"
   uci set network.@wireguard_${ifname}[-1].endpoint_host="$fastest_server"
   uci commit network
   /etc/init.d/network restart
