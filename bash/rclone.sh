02631269b2846087753f64d93301f9de6efac4b3
lukaskronus <79519320+lukaskronus@users.noreply.github.com>
2024-06-14 10:53:21 +0700

    Update rclone.sh

--- bash/rclone.sh
+++ bash/rclone.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 # rclone sync to destinations
-docker exec -it rclone rclone sync ubytxh:Shared/Ebooks/Requested/"Anh Thuyen Ebooks" onedrive:Ebooks/Requested/"Anh Thuyen Ebooks" -P --ignore-existing --track-renames --fix-case --server-side-across-configs
-docker exec -it rclone rclone sync ubytxh:Shared/Ebooks/Requested/"Anh Thuyen Ebooks" /data/Ebooks -P --ignore-existing --track-renames --fix-case --server-side-across-configs
-docker exec -it rclone rclone sync ubytxh:Shared/Ebooks/Requested/"Anh Thuyen Ebooks" gdrive:Ebooks/ -P --ignore-existing --track-renames --fix-case --server-side-across-configs
+docker exec -it rclone rclone sync ubytxh:Shared/Ebooks/Requested/"Anh Thuyen Ebooks" onedrive:Ebooks/Requested/"Anh Thuyen Ebooks" -P --ignore-existing --track-renames --fix-case --server-side-across-configs --check-first
+docker exec -it rclone rclone sync ubytxh:Shared/Ebooks/Requested/"Anh Thuyen Ebooks" gdrive:Ebooks/ -P --ignore-existing --track-renames --fix-case --server-side-across-configs --check-first
+docker exec -it rclone rclone sync ubytxh:Shared/Ebooks/Requested/"Anh Thuyen Ebooks" /data/Ebooks -P --ignore-existing --track-renames --fix-case --server-side-across-configs --check-first
+
 
 # check data from source and destinations
 docker exec -it rclone rclone size ubytxh:Shared/Ebooks/Requested/"Anh Thuyen Ebooks"
