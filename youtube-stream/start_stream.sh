940315915d5a9d02da3079d2274dc5aa3e7fc115
lukaskronus <79519320+lukaskronus@users.noreply.github.com>
2026-05-09 08:22:04 +0700

    Update start_stream.sh for continuous streaming
    Modify the streaming command to use a loop for automatic restarts and silence audio.

--- youtube-stream/start_stream.sh
+++ youtube-stream/start_stream.sh
@@ -5,7 +5,19 @@ RTSP_LINK=""
 STREAM_KEY=""
 
 # Start the streaming with FFmpeg (no video encoding/decoding, silent audio, output to /dev/null) and save the PID to /tmp/pid.txt
-ffmpeg -i "$RTSP_LINK" -f lavfi -i anullsrc=channel_layout=stereo:sample_rate=44100 -c:v copy -c:a aac -shortest -f flv "rtmp://a.rtmp.youtube.com/live2/$STREAM_KEY" > /dev/null 2>&1 &
+while true; do
+  nice -n 10 ffmpeg \
+    -rtsp_transport tcp \
+    -i "$RTSP_LINK" \
+    -stream_loop -1 -i silence.aac \ # Use silence.aac instead for audio
+    -c:v copy \
+    -c:a copy \
+    -threads 2 \
+    -f flv "rtmp://a.rtmp.youtube.com/live2/$STREAM_KEY" \
+    > /dev/null 2>&1
+  echo "Stream crashed, restarting in 5s..."
+  sleep 5
+done &
 
 # Save the PID of the FFmpeg process to /tmp/pid.txt
 echo $! > /tmp/pid.txt
