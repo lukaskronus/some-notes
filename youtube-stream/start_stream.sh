09b39f2682b1a4b5c63a417bef84e7d6323e7e75
lukaskronus <79519320+lukaskronus@users.noreply.github.com>
2026-05-28 16:24:39 +0700

    Update Youtube streaming shell scripts

--- youtube-stream/start_stream.sh
+++ youtube-stream/start_stream.sh
@@ -1,15 +1,16 @@
-#!/bin/bash
-
-# Set the RTSP link of your IP camera and YouTube stream key
+#!/bin/sh
 RTSP_LINK=""
 STREAM_KEY=""
 
-# Start the streaming with FFmpeg (no video encoding/decoding, silent audio, output to /dev/null) and save the PID to /tmp/pid.txt
+# Kill any leftover ffmpeg from a previous session before starting
+pkill -f ffmpeg
+sleep 2
+
 while true; do
   nice -n 10 ffmpeg \
     -rtsp_transport tcp \
     -i "$RTSP_LINK" \
-    -stream_loop -1 -i silence.aac \ # Use silence.aac instead for audio
+    -stream_loop -1 -i silence.aac \
     -c:v copy \
     -c:a copy \
     -threads 2 \
@@ -19,7 +20,5 @@ while true; do
   sleep 5
 done &
 
-# Save the PID of the FFmpeg process to /tmp/pid.txt
 echo $! > /tmp/pid.txt
-
 echo "Streaming started. PID: $(cat /tmp/pid.txt)"
--- youtube-stream/stop_stream.sh
+++ youtube-stream/stop_stream.sh
@@ -1,2 +1,5 @@
-#!/bin/bash
-[ -f /tmp/pid.txt ] && kill $(cat /tmp/pid.txt) && rm /tmp/pid.txt
+#!/bin/sh
+# Kill the saved while-loop PID if it exists
+[ -f /tmp/pid.txt ] && kill $(cat /tmp/pid.txt) 2>/dev/null && rm /tmp/pid.txt
+# Also kill any lingering ffmpeg processes
+pkill -f ffmpeg
